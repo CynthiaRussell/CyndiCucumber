aa6d6cdc2b4773a2376054cfb82cc5ec591a5fd5
cyndi.russell <cyndirussell2012@gmail.com>
2016-05-05 15:28:09 -0700

    First Commit

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by cyndi.russell on 5/4/16.
+ */
+public class MyFirstTest {
+
+    @Test
+    public void goToWebPage(){
+
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://www.mozilla.org/en-US/firefox/45.0.2/firstrun/learnmore/");
+        assertTrue(driver.getTitle().equals("mozilla"));
+
+        driver.quit();
+    }
+
+
+    }
+
